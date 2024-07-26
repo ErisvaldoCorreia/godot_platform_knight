4c402a3f9de85f53ff71ed7e5aa49599388e1d72
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2024-07-26 17:36:44 -0300

    adding hud canvas

--- scripts/gamemanager.gd
+++ scripts/gamemanager.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 var points_coins = 0
-@onready var score_label = $scoreLabel
+@onready var scorepoint = $hud/scorepoint
 
 func add_point():
 	points_coins += 1
-	score_label.text = "Parabéns, Coletou " + str(points_coins) + " moedas"
+	scorepoint.text = str(points_coins)
 	print("placar: " ,points_coins)
 
