7ef1654ff1ff3c6fe9a7a4522b717aa060454073
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2024-07-26 12:55:12 -0300

    adding enemy and fix killzone bug

--- scripts/kilzone.gd
+++ scripts/kilzone.gd
@@ -3,10 +3,9 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(_body):
-	print('Você morreu')
+	print('você morreu')
 	timer.start()
 
 
 func _on_timer_timeout():
-	get_tree().reload_current_scene() 
-
+	get_tree().reload_current_scene()
